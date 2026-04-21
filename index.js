07be03d031c98bcc1ad19da4e4de7e4bbb7a1852
sheikh umar <shaykhomr@gmail.com>
2026-04-21 17:39:15 +0530

    we have added new file ---> intro.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+//here we are not using git add index.js 
+//git bash will show untracked till now
 console.log("hello world ");
-console.log("we are just mimic the feature of git");
+console.log("we are just mimic the feature of git ");
+//after using git add index.js we make changes
+//and git will track our changes now
+//after this git bash will show changes to be committed
+//also show new file and modified :index.js
+console.log("now we are changing somehting");
